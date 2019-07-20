2c8103b510cc6582f258a72e62a47b3c5eb6fbd7
Michael Vega <ichigo509@yahoo.com>
2019-07-20 09:14:07 -0400

    initial commit with everything setup

---
+++ app.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAOn2cm3l005arGIY2DBX6Ccirw-5o6lHI",
+    authDomain: "test-project-e55be.firebaseapp.com",
+    databaseURL: "https://test-project-e55be.firebaseio.com",
+    projectId: "test-project-e55be",
+    storageBucket: "",
+    messagingSenderId: "31934415382",
+    appId: "1:31934415382:web:a3b0c0e083f23646"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var datebase = firebase.database();
+
+  
